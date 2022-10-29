e43b1909e33b1f43e06df521c6f21f1edb272a45
Blatko1 <kings.of.funny.gemplays@gmail.com>
2022-10-29 21:46:41 +0200

    more ui

--- src/interface.rs
+++ src/interface.rs
@@ -1,19 +1,20 @@
-use std::io;
+use std::{io, marker::PhantomData};
 
 use crossterm::event::KeyEvent;
 use tui::{
     backend::Backend,
-    layout::{Alignment, Constraint, Corner, Direction, Layout},
+    layout::{Alignment, Constraint, Corner, Direction, Layout, Rect},
     terminal::CompletedFrame,
     text::{Span, Spans},
     widgets::{Block, BorderType, Borders, List, ListItem, Paragraph},
     Frame, Terminal,
 };
 
-use crate::input::{self, InputHandler, Interruption};
+use crate::input::{InputHandler, Interruption};
 
 pub struct Interface<B: Backend> {
     terminal: Terminal<B>,
+    ui: UI<B>,
     input: InputHandler,
     input_mode: InputMode,
     exit: bool,
@@ -23,6 +24,7 @@ impl<B: Backend> Interface<B> {
     pub fn new(terminal: Terminal<B>) -> Self {
         Self {
             terminal,
+            ui: UI::new(),
             input: InputHandler::new(),
             input_mode: InputMode::Editing,
             exit: false,
@@ -31,53 +33,8 @@ impl<B: Backend> Interface<B> {
 
     pub fn render_ui(&mut self) -> io::Result<CompletedFrame> {
         let size = self.terminal.size().unwrap();
-        let ui = |frame: &mut Frame<B>| {
-            // Split the terminal into 2 parts left and right:
-            let left_right_main = Layout::default()
-                .direction(Direction::Horizontal)
-                .margin(1)
-                .constraints(
-                    [Constraint::Percentage(50), Constraint::Percentage(50)]
-                        .as_ref(),
-                )
-                .split(size);
-            // Split the right part into a big one on top and the small one on bottom:
-            let top_bottom_second = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(0)
-                .constraints(
-                    [Constraint::Percentage(90), Constraint::Percentage(10)]
-                        .as_ref(),
-                )
-                .split(left_right_main[1]);
 
-            // ____________RENDER STUFF ONTO THE RIGHT PART OF THE TERMINAL: ____________
-            let items = vec![
-                ListItem::new("mama ti!"),
-                ListItem::new("tata ti!"),
-                ListItem::new("baka ti!"),
-            ];
-            let alerts_list =
-                List::new(items).start_corner(Corner::BottomLeft).block(
-                    Block::default()
-                        .title("Alerts List")
-                        .title_alignment(Alignment::Center)
-                        .borders(Borders::all())
-                        .border_type(BorderType::Rounded),
-                );
-            frame.render_widget(alerts_list, top_bottom_second[0]);
-
-            let text = vec![Spans::from(vec![Span::from("Market price:")])];
-            let price_paragraph = Paragraph::new(text).block(
-                Block::default()
-                    .title("Live Price")
-                    .border_type(BorderType::Double)
-                    .borders(Borders::all()),
-            );
-            frame.render_widget(price_paragraph, top_bottom_second[1]);
-        };
-
-        self.terminal.draw(ui)
+        self.terminal.draw(|f| self.ui.draw(f, size))
     }
 
     pub fn process_controls(&mut self, event: KeyEvent) {}
@@ -105,6 +62,100 @@ impl<B: Backend> Interface<B> {
 
     pub fn should_exit(&self) -> bool {
         self.exit
+    }
+}
+
+struct UI<B: Backend> {
+    phantom: PhantomData<B>,
+}
+
+impl<B: Backend> UI<B> {
+    fn new() -> Self {
+        Self {
+            phantom: PhantomData,
+        }
+    }
+
+    fn draw(&self, frame: &mut Frame<B>, size: Rect) {
+        // Split the terminal into 2 parts left and right:
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(2)
+            .constraints([
+                Constraint::Percentage(50),
+                Constraint::Percentage(50),
+            ])
+            .split(size);
+
+        // ____________RENDER STUFF ONTO THE RIGHT PART OF THE TERMINAL: ____________
+        self.draw_left_half(frame, chunks[0]);
+        self.draw_right_half(frame, chunks[1]);
+    }
+
+    fn draw_left_half(&self, frame: &mut Frame<B>, area: Rect) {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Max(7), Constraint::Min(6)])
+            .split(area);
+        
+        self.draw_input_chunk(frame, chunks[0]);
+        self.draw_else(frame, chunks[1]);
+    }
+
+    fn draw_right_half(&self, frame: &mut Frame<B>, area: Rect) {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([
+                Constraint::Percentage(90),
+                Constraint::Percentage(10),
+            ])
+            .split(area);
+
+        let alert_items = vec![
+            ListItem::new("mama ti!"),
+            ListItem::new("tata ti!"),
+            ListItem::new("baka ti!"),
+        ];
+        let alerts_list = List::new(alert_items)
+            .start_corner(Corner::BottomLeft)
+            .block(
+                Block::default()
+                    .title("Alerts List")
+                    .title_alignment(Alignment::Center)
+                    .borders(Borders::all())
+                    .border_type(BorderType::Thick),
+            );
+        frame.render_widget(alerts_list, chunks[0]);
+
+        let market_price = vec![Spans::from(vec![Span::from("Market price:")])];
+        let price_paragraph = Paragraph::new(market_price).block(
+            Block::default()
+                .title("Live Price")
+                .border_type(BorderType::Double)
+                .borders(Borders::all()),
+        );
+        frame.render_widget(price_paragraph, chunks[1]);
+    }
+
+    fn draw_input_chunk(&self, frame: &mut Frame<B>, area: Rect) {
+        let input_chunk = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Length(1), Constraint::Min(3)])
+            .split(area);
+
+        let block = Block::default()
+            .borders(Borders::all())
+            .border_type(BorderType::Plain)
+            .title("Add Zones");
+        frame.render_widget(block, area);
+
+        let 
+    }
+
+    // TODO
+    fn draw_else(&self, frame: &mut Frame<B>, area: Rect) {
+        let block = Block::default().borders(Borders::all());
+        frame.render_widget(block, area);
     }
 }
 
